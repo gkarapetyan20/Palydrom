d714e8dabfd90ef02a0eb43e1352b5af296546bb
gkarapetyan20 <87021325+gkarapetyan20@users.noreply.github.com>
2021-07-11 00:48:52 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "pal.hpp"
+using namespace std;
+int main() {
+  
+          Palydrom();
+          return 0;
+  }
+        
---
+++ pal.hpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <string>
+int Palydrom();
+int main();
